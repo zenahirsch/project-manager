9f0c233df55fa2602f9104a0f9c37c4651ef1170
Zena Hirsch <hirsch.zena@gmail.com>
2016-02-15 21:13:13 -0500

    move static use statement

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static('./src/static'));
+
 // Middleware
 app.use(handlebars);
 app.use(database);
 
 // Routes
 app.use('/', index);
-
-app.use(express.static('./src/static'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
